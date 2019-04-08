01987cb67c5cb5e5c47fc45d020699b2faa44919
Klynbrsh <kailynb3@gmail.com>
2019-04-08 16:23:04 -0600

    Account Creation Form js

--- index.js
+++ index.js
@@ -1,13 +1,144 @@
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+// form1.pass1.value = "";
+//         form1.pass2.value = "";
+//         form1.pass1.focus();
+
+
+function checkForm()
+{
+	var firstn = document.getElementById('firstn').value;
+	var lastn = document.getElementById('lastn').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;
+    var confirm_password = document.getElementById('conpass').value;
+    var terms_ok = document.getElementById('Terms').checked;
+    var black = document.getElementById('Black').checked;
+    var white = document.getElementById('White').checked;
+    var oolong = document.getElementById('Oolong').checked;
+    var green = document.getElementById('Green').checked;
+    var dark = document.getElementById('Dark').checked;
+    var unsure = document.getElementById('Unsure').checked;
+	
+	
+	var form_ok = true;
+	
+	var fname_ok = true;
+
+    var lname_ok = true;
+
+    var pass_ok = false; 
+	
+    var email_ok = true;
+
+	if (email == "")
+	{
+        email_ok = false;
+	}
+	else
+	{
+		var atpos = email.indexOf('@');
+		var dotpos = email.indexOf('.');
+		
+		if (atpos < 1)
+		{
+            email_ok = false;
+            alert ("Please provide a valid email address.");
+		}
+		else
+		{
+			if (email.length-dotpos < 1)
+			{
+                email_ok = false;
+                alert ("Please provide a valid email address.")
+			}
+			else
+			{
+				if (dotpos-atpos < 2)
+				{
+                    email_ok = false;
+                    alert ("Please provide a valid email address.")
+				}
+			}
+		}
     }
-  }
-// If (validateForm()==false) {
-//     document.getElementById("submit").disabled = true;
+    
+        if (password == "")
+            {
+                alert("Please provide a password.");
+                pass_ok = false;
+            }
+        else {
+            if (confirm_password == "")
+            {
+                alert("Please confirm your password.");
+                pass_ok = false;
+            }
+            else{
+    
+                if(String(password) != String(confirm_password)) 
+                {
+                    alert ("Passwords Don't Match");
+                    pass_ok = false;
+                } else {
+                    pass_ok = true;
+                }
+            }
+            
+        }
+
+    if(black == true) 
+    {
+        tea_ok = true;
+    }
+    else if (green == true)
+    {
+        tea_ok = true;
+    }
+    else if (white == true)
+    {
+        tea_ok = true;
+    }
+    else if (oolong == true)
+    {
+        tea_ok = true;
+    }
+    else if (dark == true)
+    {
+        tea_ok = true;
+    }else if (unsure == true)
+    {
+        tea_ok = true;
+    }
+    else {
+        tea_ok = false
+    }
+// function check() {
+//     document.getElementById("myCheck").checked = true;
 // }
-//   else (validateForm()==true) {
-//     document.getElementById("Button").disabled = false;
-//   }
+
+// function uncheck() {
+//     document.getElementById("myCheck").checked = false;
+// }
+        
+
+    form_ok = (lname_ok && email_ok && pass_ok && fname_ok && terms_ok && tea_ok);
+    if (!form_ok)
+	{
+		
+        if(!tea_ok)
+		{
+			alert("Please choose a favorite tea.");
+        }
+         else
+		{
+            if (!terms_ok)
+            {
+                alert("Please agree to our terms and services.");
+            }
+        }
+    }
+
+	return form_ok;
+}
+
+    
+   
